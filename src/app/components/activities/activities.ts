fa3cf352bd83759e3b7663f740cfee6b156106fc
User <user@example.com>
2026-03-02 13:24:59 -0500

    Cleanup: Remove Angular and unused config files

--- src/app/components/activities/activities.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-activities',
-  imports: [],
-  templateUrl: './activities.html',
-  styleUrl: './activities.css',
-})
-export class Activities {}
